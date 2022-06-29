4c9c99608a91c1be82ef703acf246141a720434e
moustafamorsy <mostafamorsyyy@gmail.com>
2022-06-29 11:06:08 +0200

    learn2

--- Script.js
+++ Script.js
@@ -1,43 +1,38 @@
 
-let store =  [3, 1, 7, 9]
+let store =  [3, 1, 7, 9 ]
 
 let store1 = [2, 4, 1, 9, 3]
 
 
-// remove all ducplication and add all the unique value 
+// ignore all ducplication and add all the unique value 
 
 let all_store = [...store , ...store1]
 
  
-   let mw =  all_store.filter((item,
-        index) => all_store.indexOf(item) === index);
-       
-var count = 0 ;
+const addunique = arr => {
+   let sum = 0;
+   for(let i = 0; i < arr.length; i++){
+      if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])){
+         sum += arr[i];
 
-mw.forEach(element => {
-  
-     count = count + element ;
-  });
-  console.log(count); 
-
+      };
+   };
+   return sum;
+};
+console.log(addunique(all_store));
    
 // add only duplicate values
 
 
- var counts = 0 ;
+ 
+const aaddunique = arr => {
+   let sum = 0;
+   for(let i = 0; i < arr.length; i++){
+      if(arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])){
+         sum += arr[i];
 
-for (let i = 0; i < store.length; i++) {
-   for (let j = 0; j < store1.length; j++) {
-
-   if (store[i] === store1[j]) {
-
-    var counts = counts + store[i] + store1[j] ;
-   
-   } 
-
-   }
-   
-
-}
-
-console.log(counts);
+      };
+   };
+   return sum;
+};
+console.log(aaddunique(all_store));
